ec719297eea3890bbfa7adf33eabe2055b83533f
andreabrg15 <andrea.reynagz@uanl.edu.mx>
2026-06-30 17:31:19 -0600

    Envio de correos de contacto -LISTO

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,49 @@
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 
 function Contact() {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+        .sendForm("contact_service", "contact_form", form.current, {
+            publicKey: "B8HDAOMizhuL_I7NA"
+        }).then(
+            () => {
+                alert("Se envio su correo con exito");
+            },
+            (error) => {
+                console.log("Error", error.text);
+            }
+        );
+    }
+
     return (
-        <div className="grid nunito-sans font-bold text-lg p-5">
+        <div className="grid nunito-sans font-bold text-lg p-3">
             <h1 className="text-5xl changa-one-regular text-white">Contacto</h1>
             <div className="grid grid-cols-8">
                 {/*Formulario de contacto*/}
                 <div className="col-start-3 col-span-3 py-3">
-                    <form className="grid justify-center py-3">
-                        <label for="name" className="flex justify-start text-white mb-3">Ingrese su nombre:</label>
-                        <input className="bg-white/80 rounded-2xl w-120 px-7 py-2 mb-5" placeholder="Nombre" type="text" id="name" name="name"/>
-                        <label for="email" className="flex justify-start text-white mb-3">Ingrese su correo:</label>
-                        <input className="bg-white/80 rounded-2xl w-120 px-7 py-2 mb-5" placeholder="Correo" type="email" id="email" name="email"/>
-                        <label for="msg" className="flex justify-start text-white mb-3">Ingrese el mensaje que desea enviar:</label>
-                        <textarea className="bg-white/80 rounded-2xl w-120 px-7 py-2 mb-7" placeholder="Mensaje" rows="5" id="msg" name="msg"/>
+                    <form ref={form} className="grid justify-center py-3 gap-2" onSubmit={sendEmail}>
+                        <label className="flex justify-start text-white">Ingrese su nombre:</label>
+                        <input className="bg-white/80 rounded-2xl w-120 px-7 py-2" placeholder="Nombre" type="text" id="name" 
+                        name="name" required/>
+                        <label className="flex justify-start text-white">Ingrese su correo:</label>
+                        <input className="bg-white/80 rounded-2xl w-120 px-7 py-2" placeholder="Correo" type="email" id="email" 
+                        name="from_email" required/>
+                        <label className="flex justify-start text-white">Ingrese el asunto:</label>
+                        <input className="bg-white/80 rounded-2xl w-120 px-7 py-2" placeholder="Asunto" type="text" id="title" 
+                        name="title" required/>
+                        <label className="flex justify-start text-white">Ingrese el mensaje que desea enviar:</label>
+                        <textarea className="bg-white/80 rounded-2xl w-120 px-7 py-2 mb-5" placeholder="Mensaje" rows="4" id="message" 
+                        name="message" required/>
                         <button className="bg-mauve-900 text-white rounded-3xl nunito-sans font-bold px-10 
-                        py-3 hover:bg-mauve-900/70 hover:cursor-pointer">
+                        py-3 hover:bg-mauve-900/70 hover:cursor-pointer" type="submit">
                             Enviar
                         </button>
                     </form>
