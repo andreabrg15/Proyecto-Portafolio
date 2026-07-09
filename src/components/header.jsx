795d94d7a0ee59fc4979470c2a53daa40d333a82
andreabrg15 <andrea.reynagz@uanl.edu.mx>
2026-07-08 19:34:57 -0600

    Inicio Responsivo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -17,22 +17,24 @@ function Header() {
 
     return (
         <>
-            <section className="h-24 grid grid-cols-6 items-center nunito-sans font-bold text-lg text-white">
-                <div className="col-start-5 col-span-2 underline underline-offset-4">
+            <section className="lg:h-24 grid lg:grid-cols-6 items-center nunito-sans font-bold text-lg text-white">
+                <div className="grid py-5 gap-3 lg:py-0 lg:col-start-5 lg:col-span-2 lg:flex lg:gap-8">
                     {
-                    (location.pathname != '/') && <Link className="px-5 hover:text-fuchsia-500" to="/">Inicio</Link>
+                    (location.pathname != '/') && <div className="underline underline-offset-4"><Link className="hover:text-fuchsia-500" to="/">Inicio</Link></div>
                     }
                     {
-                    (location.pathname != '/proyectos') && <Link className="px-5 hover:text-fuchsia-500" to="/proyectos">Proyectos</Link>
+                    (location.pathname != '/proyectos') && <div className="underline underline-offset-4"><Link className="hover:text-fuchsia-500" to="/proyectos">Proyectos</Link></div>
                     }
                     {
-                    (location.pathname != '/contacto') && <Link className="px-5 hover:text-fuchsia-500" to="/contacto">Contacto</Link>
+                    (location.pathname != '/contacto') && <div className="underline underline-offset-4"><Link className="hover:text-fuchsia-500" to="/contacto">Contacto</Link></div>
                     }
-                    <button onClick={downloadCV} 
-                    className="bg-mauve-900 text-white rounded-3xl nunito-sans font-bold px-4 py-1 
-                    hover:bg-mauve-900/70 hover:cursor-pointer hover:text-fuchsia-500">
-                        Descargar CV
-                    </button>
+                    <div>
+                        <button onClick={downloadCV} 
+                        className="bg-mauve-900 text-white rounded-3xl nunito-sans font-bold px-4 py-1 
+                        hover:bg-mauve-900/70 hover:cursor-pointer hover:text-fuchsia-500">
+                            Descargar CV
+                        </button>
+                    </div>
                 </div>
             </section>
         </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,28 +6,34 @@ import { FaAngular } from "react-icons/fa";
 function Home() {
     return (
         <>
-            <div className="grid grid-cols-6 p-5 animate-appear">
-                <div className="col-span-2 pt-6 pl-24 nunito-sans">
-                    <img src="/20260617.jpg" className="justify-self-center size-48 rounded-full" alt="Foto"/>
-                    <h1 className="text-5xl text-white changa-one-regular pt-4">Andrea Reyna</h1>
-                    <h3 className="text-2xl text-white pt-2">Desarrolladora Jr</h3>
-                </div>
-                <div className="col-span-4 pt-22">
-                    <p className="text-justify nunito-sans font-bold text-white text-lg/8 pl-20 pr-30">
-                        Soy una desarrolladora jr, con conocimientos en React, Node.js y .NET, egresada de la Facultad
-                        de Ciencias Físico Matemáticas. Me interesa crear soluciones funcionales que brinden una buena
-                        experiencia de usuario y me gusta aprender cosas nuevas. He trabajado con operaciones CRUD,
-                        consumo y desarrollo de APIs, así como con bases de datos SQL a través de proyectos personales
-                        y académicos. Actualmente, busco una oportunidad para construir mayor experiencia en la industria
-                        del desarrollo de manera profesional.
-                    </p>
+            <div className="grid px-5 py-2 lg:p-5 animate-appear">
+                <div className="grid justify-items-center lg:flex lg:pl-25 lg:pr-10">
+                    <div className="grid nunito-sans lg:w-1/4 lg:pt-7">
+                        <img src="/20260617.jpg" className="justify-self-center size-48 rounded-full" alt="Foto"/>
+                        <h1 className="text-5xl text-white changa-one-regular pt-4">Andrea Reyna</h1>
+                        <h3 className="text-2xl text-white pt-2">Desarrolladora Jr</h3>
+                    </div>
+                    <div className="pt-7 lg:pt-17 lg:w-3/4">
+                        <p className="text-justify nunito-sans font-bold text-white text-lg/8 px-5 lg:px-20">
+                            Soy una desarrolladora jr, con conocimientos en React, Node.js y .NET, egresada de la Facultad
+                            de Ciencias Físico Matemáticas. Me interesa crear soluciones funcionales que brinden una buena
+                            experiencia de usuario y me gusta aprender cosas nuevas. He trabajado con operaciones CRUD,
+                            consumo y desarrollo de APIs, así como con bases de datos SQL a través de proyectos personales
+                            y académicos. Actualmente, busco una oportunidad para construir mayor experiencia en la industria
+                            del desarrollo de manera profesional.
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div className="grid grid-cols-6 p-5 pt-5 pb-8 animate-appear">
-                <IoLogoReact size={100} color="#00d3f3" className="animate-spin col-start-2 justify-self-center"/>
-                <FaAngular size={100} color="#fb2c36" className="justify-self-center"/>
-                <IoLogoJavascript size={100} color="#ffff00" className="bg-black rounded-lg justify-self-center"/>
-                <SiDotnet size={100} color="#fff" className="bg-violet-600 rounded-lg justify-self-center"/>
+            <div className="grid justify-items-center gap-y-5 md:grid-cols-6 p-5 pt-5 pb-8 animate-appear text-xs xl:text-sm">
+                <div className="flex gap-10 md:gap-x-7 lg:gap-x-17 xl:gap-x-25 2xl:gap-x-35 md:col-start-2 md:col-span-2">
+                    <IoLogoReact size={"8em"} color="#00d3f3" className="animate-spin"/>
+                    <FaAngular size={"8em"} color="#fb2c36"/>
+                </div>
+                <div className="flex gap-10 md:gap-x-7 lg:gap-x-17 xl:gap-x-25 2xl:gap-x-35 md:col-span-2">
+                    <IoLogoJavascript size={"8em"} color="#ffff00" className="bg-black rounded-lg"/>
+                    <SiDotnet size={"8em"} color="#fff" className="bg-violet-600 rounded-lg"/>
+                </div>
             </div>
             <div className="grid grid-cols-6 p-5 animate-appear">
                 <Link className="bg-mauve-900 text-white rounded-3xl nunito-sans font-bold px-10 py-3 col-start-3 
